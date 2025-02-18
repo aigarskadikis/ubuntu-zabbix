1c7854cbc0ff1f9c1b730d94f504c422000d7783
aigarskadikis <aigars.kadikis@gmail.com>
2025-02-18 16:51:52 +0200

    included MSSQL ODBC

--- prx.sh
+++ prx.sh
@@ -153,3 +153,23 @@ sudo systemctl enable zabbix-java-gateway
 # proxy, agent, gateway must be in listening state
 ss --tcp --listen --numeric | grep -E "(10051|10050|10052)"
 
+# setup ODBC driver for PostgreSQL
+sudo apt -y install odbc-postgresql
+
+# install MS SQL ODBC
+# Download the package to configure the Microsoft repo
+curl -sSL -O https://packages.microsoft.com/config/ubuntu/$(grep VERSION_ID /etc/os-release | cut -d '"' -f 2)/packages-microsoft-prod.deb
+# Install the package
+sudo dpkg -i packages-microsoft-prod.deb
+# Delete the file
+rm packages-microsoft-prod.deb
+
+# Install the driver
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+# optional: for bcp and sqlcmd
+sudo ACCEPT_EULA=Y apt-get install -y mssql-tools18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
